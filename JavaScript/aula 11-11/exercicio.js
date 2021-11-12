d28153226607f91ecdcc7ce87e076e48134833d9
JenniferPP <jenni-pp@hotmail.com>
2021-11-11 21:55:40 -0300

    Exercicios JS

--- JavaScript/aula 11-11/exercicio.js
+++ JavaScript/aula 11-11/exercicio.js
@@ -8,6 +8,19 @@ class contaBancaria{
         this.conta = conta;
         this.tipoConta = tipoConta;
     }
-
     
+    contaPoupanca(tipoConta){
+        return this.salario * 1.5;
+    };
+
+    contCorrente(tipoConta){
+        return this.salario * 3.6;
+    };
+
+    contaEstudante(tipoConta){
+        return this.salario * 1.2;
+    };
 }
+
+let dadosUser = new contaBancaria ('Pheeb', '34', 1500, 'femino', '12345-6', '78910', contaEstudante());
+let descSalario =

2bb5d0dadc2e553f2c7ecb6c0225b3e4362cc6b1
JenniferPP <jenni-pp@hotmail.com>
2021-12-15 22:01:25 -0300

    Atividade da pesquisa

---
+++ JAVA/Demonstracao_Matriz/src/Demonstracao.java
@@ -0,0 +1,20 @@
+public class Demonstracao {
+    public static void main(String[] args){
+        String nomes[] = {"Heloisa", "Duda", "Rafa"}; // Array
+
+        // Forma incorreta de declarar uma Matriz
+        //String nome[][]= {{"Heloisa", "Helo", 21}, {"Duda", "DU", 32}, {"Rafa", "Ra", 35}} //Matriz
+
+        //Forma correta de declarar uma Matriz
+        // TIPO DE ICENTIFICADOR [][] = NEW TIPO {{TamLinha}, {TamCol}}
+
+        double numeros[][] = new double[3][3];
+
+        int numeros2[][] = new int[3][50];
+
+        int notas[][] ={{8,9,9}, {4,7,9},{2,5,7}};
+
+
+
+    }
+}
